a261eb49a2a176625cdfff6ef4cf5e7b7166c3dd
Fábio Senra Corrêa <fabiosenracorrea@hotmail.com>
2020-11-03 20:29:24 -0300

    incorrect folder deletion

--- Course_Lectures/nodeJS_designing_our_backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import appointmentsRoute from './appointments.routes';
-
-const routes = Router();
-
-routes.use('/appointments', appointmentsRoute);
-
-export default routes;
